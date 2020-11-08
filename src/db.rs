079e255272e711b51bf2f12900ea48e7ab7e81c1
kylepo99 <kyle.haddad@icloud.com>
2020-11-08 00:31:28 -0600

    hi

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use postgres::{Client, NoTls,Error,Row};
 
 pub fn test() -> Result<(), Error> {
-    let mut client = Client::connect("postgresql://postgres:admin@localhost/user_data", NoTls)?;
+    let mut client = Client::connect("postgresql://DEFAULT@localhost/user_data", NoTls)?;
     client.batch_execute("
     CREATE TABLE accounts (
         id SERIAL PRIMARY KEY,
@@ -14,7 +14,7 @@ Ok(())
 }
 
 pub fn create_user_acount(username: String,password: String) -> Result<(), Error> {
-    let mut client = Client::connect("postgresql://postgres:Aadmin@localhost/user_data", NoTls)?;
+    let mut client = Client::connect("postgresql://DEFAULT@localhost/user_data", NoTls)?;
     client.execute(
                 "INSERT INTO accounts (username, password) VALUES ($1, $2)",
                 &[&username, &password],
@@ -24,7 +24,7 @@ pub fn create_user_acount(username: String,password: String) -> Result<(), Error
 }
 
 pub fn give_user_token(username: String,token: &String) -> Result<(), Error> {
-    let mut client = Client::connect("postgresql://postgres:admin@localhost/user_data", NoTls)?;
+    let mut client = Client::connect("postgresql://DEFAULT@localhost/user_data", NoTls)?;
     client.execute(
                 "UPDATE accounts SET token = ($1) WHERE username = ($2)",
                 &[&token, &username],
@@ -33,7 +33,7 @@ pub fn give_user_token(username: String,token: &String) -> Result<(), Error> {
 }
 
 pub fn find_token(token: String) -> Result<Vec<Row>, Error> {
-    let mut client = Client::connect("postgresql://postgres:admin@localhost/user_data", NoTls)?;
+    let mut client = Client::connect("postgresql://DEFAULT@localhost/user_data", NoTls)?;
     let row = client.query("SELECT token FROM accounts WHERE token = ($1)", &[&token])?;
     println!("{:?}",row);
     Ok(row)
@@ -43,7 +43,7 @@ pub fn find_token(token: String) -> Result<Vec<Row>, Error> {
 
 pub fn find_user_account(username: String) -> Result<Vec<Row>, Error> {
     println!("running");
-    let mut client = Client::connect("postgresql://postgres:admin@localhost/user_data", NoTls)?;
+    let mut client = Client::connect("postgresql://DEFAULT@localhost/user_data", NoTls)?;
     let row = client.query("SELECT password FROM accounts WHERE username = ($1)", &[&username])?;
     Ok(row)
 }
